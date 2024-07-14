9edd15545755e68ddbc73b5ea2e595bf38ee5896
hossam <hossamomar2nd@gmail.com>
2024-07-14 22:08:30 +0300

    add dotenv pakage to import .env variables

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { DBconnection } from "./Database/connection.js";
 import { bootstrab } from "./src/modules/index.routes.js";
+import dotenv from "dotenv";
 ////////////////////////////////////////////////////////////////////////
-
+dotenv.config();
 const app = express();
 const port = 3000;
 app.use(express.json());
